dd2387d14040903115e891d83263e78f9e9d4f79
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-03-30 12:14:23 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,4 +1,37 @@
 
+const withPlugins = require('next-compose-plugins');
+const withTM = require('next-transpile-modules')(['module1', 'module2']);
+const TerserPlugin = require('terser-webpack-plugin');
+
+module.exports = withPlugins([withTM], {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer = [new TerserPlugin()];
+    }
+    return config;
+  },
+});
+
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withBundleAnalyzer({
+  // your Next.js configuration
+  webpack: (config, { isServer }) => {
+    // custom webpack configuration
+    return config;
+  },
+});
+
+const withOptimizedImages = require('next-optimized-images')
+
+module.exports = withOptimizedImages({
+  optimizeImagesInDev: true,
+  // Other Next.js configurations
+})
+
+
 module.exports = {
     async headers() {
       return [
