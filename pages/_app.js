e3ba7252c6afc86564f55ac071a21a92d6c78b3d
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-03 14:21:15 +0530

    update final 3 April 2023

--- pages/_app.js
+++ pages/_app.js
@@ -3,16 +3,14 @@ import '@styles/globals.css'
 import Footer from '../components/Footer';
 import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
-import React from 'react';
-import ReactGA from 'react-ga';
+import GoogleAnalytics from "@bradgarropy/next-google-analytics"
+
 
 function Application({ Component, pageProps }) {
-  React.useEffect(() => {
-    ReactGA.initialize('G-K8QKRZ4B44');
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
+ 
   return ( 
-
+    <>
+    <GoogleAnalytics measurementId="G-K8QKRZ4B44" />
       <PageTransition>  
   <Hamburger />
 
@@ -22,7 +20,7 @@ function Application({ Component, pageProps }) {
   <Footer />
 
    </PageTransition>
-  
+   </>
 
   );
 }
