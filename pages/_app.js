5513ec28b48e8f8be553a79b17535748a68b2f42
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-12 18:31:03 +0530

    updated App js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-
 import '@styles/globals.css'
 import Footer from '../components/Footer';
 import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics"
-
+import Head from 'next/head';
 
 function Application({ Component, pageProps }) {
  
@@ -12,18 +11,16 @@ function Application({ Component, pageProps }) {
     <>
      <div className="center">
     <GoogleAnalytics measurementId="G-K8QKRZ4B44" />
+      <Head>
+      <script async data-id="101405628" src="//static.getclicky.com/js"></script>
+      </Head>
       <PageTransition>  
-  <Hamburger />
-
-  
-  <Component {...pageProps} />
-
-  <Footer />
-
-   </PageTransition>
+        <Hamburger />
+        <Component {...pageProps} />
+        <Footer />
+      </PageTransition>
    </div>
    </>
-
   );
 }
 
