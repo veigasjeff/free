1314150cfa468772b616e600557f53b31ff916d8
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-14 22:08:47 +0530

    updated

--- pages/index.js
+++ pages/index.js
@@ -143,9 +143,9 @@ const schemaData   = {
       {Array.isArray(movies) && movies.slice(1).map((movie, index) => (
         <div className="w-full md:w-1/2 lg:w-1/3 p-2" key={movie.title}>
         
-        <div className="relative overflow-hidden rounded-3xl border border-white shadow-md"  width="626" height="417" >
-   
-   <img className="w-full h-full object-cover rounded-3xl border border-white shadow-md" loading="lazy" src={movie.poster} alt={movie.title} width="626" height="417" />
+        <div className="relative overflow-hidden rounded-3xl border border-white shadow-md" style={{ backgroundColor: '#f2f2f2' }}>
+    <img className="w-full h-full object-cover rounded-3xl border border-white shadow-md" loading="lazy" src={movie.poster} alt={movie.title} width="626" height="417" />
+
 
 
 
