0b0318fa2e4b619864e1211c45e73f1a13a4a9c7
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-14 14:21:34 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -3,7 +3,8 @@ import Footer from '../components/Footer';
 import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics"
-import Head from 'next/head';
+
+import Script from 'next/script';
 
 function Application({ Component, pageProps }) {
  
@@ -11,10 +12,9 @@ function Application({ Component, pageProps }) {
     <>
      <div className="center">
     <GoogleAnalytics measurementId="G-K8QKRZ4B44" />
-      <Head>
-        <script async type="text/javascript" src="//static.getclicky.com/js"></script>
-        <script async data-id="101405628" src="//static.getclicky.com/js"></script>
-      </Head>
+    <Script async type="text/javascript" src="//static.getclicky.com/js"></Script>
+    <Script async data-id="101405628" src="//static.getclicky.com/js"></Script>
+     
       <PageTransition>  
         <Hamburger />
         <Component {...pageProps} />
--- pages/index.js
+++ pages/index.js
@@ -144,7 +144,7 @@ const schemaData   = {
         <div className="w-full md:w-1/2 lg:w-1/3 p-2" key={movie.title}>
         
     <div className="relative overflow-hidden rounded-3xl border border-white shadow-md">
-  <img className="w-full h-full object-cover rounded-3xl border border-white shadow-md" loading="lazy" src={movie.poster} alt={movie.title} width="626" height="417" />
+  <img  loading="lazy" src={movie.poster} alt="loading" width="626" height="417" rel="preload" priority={true} />
   <a href={movie['movie.watch']} className="absolute inset-0 flex items-center justify-center"></a>
 
 
