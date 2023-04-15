aa072b46037c94675dfacaab728c31bfb0a54806
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-16 04:04:47 +0530

    updates now

--- pages/pages/index.js
+++ pages/pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import movies from '../public/movies.json';
 import ShareButtons from '@components/ShareButtons';
+
+
 import React, { useEffect, useState } from 'react';
 
 const scrollSearch = myKey => {
@@ -105,6 +107,9 @@ const schemaData   = {
         <meta name="revisit-after" content="1 days" />
         <meta name="facebook-domain-verification" content="13auzwhblf4oo4jn5vl6gcmebugsqb" />
         <meta property="fb:app_id" content="602176271414602" />
+        <meta name='dailymotion-domain-verification' content='dmv6sg06w9r5eji88' />
+        <meta name="monetag" content="076afbb772da1a62ef6f43756dfa5f65"></meta>
+	  	 <meta name="google-site-verification" content="4dFu4PUk1pc1IYqU6Brt84akCwNxaoUpKSO3gDW0kJ0" />
 	      <meta name="description" content="Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free" />
 	     <meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1" />
        <meta name="keywords" content="uwatchfree,streaming,latest movies,online tv,latest free movies,watch latest movies online,latest full movies online" />
@@ -122,7 +127,7 @@ const schemaData   = {
 	     <meta name="twitter:title" content="Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free " />
 	     <meta name="twitter:description" content=" Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free " />
 	     <meta name="twitter:image" content="https://uwatchfree.vercel.app/og_image.jpg" />
-       <meta name="google-site-verification" content="WQh7UdOVLh--PluVaU8U1m1IHrAPjaWmaItVOdek8tg" />
+     
       
   <link rel="icon" type="image/x-icon" href="/favicon.ico" />
   <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -133,36 +138,43 @@ const schemaData   = {
 
       </Head>
 
-      <main className={styles.main} >
-          <section className={`${styles.movies} bg-gray-600 shadow md:block py-5`} >
-            <h2 className="text-center font-bold text-3xl text-white py-5" style={{ color: "#40D7BC", textShadow: "5px 5px 2px #000" }}>Watch Movies, TV-Series & Sports Live Online Free</h2>
-            <div className="flex flex-wrap justify-center">
-              {Array.isArray(movies) && movies.slice(1).map((movie, index) => (
-                <div className="w-full md:w-1/2 lg:w-1/3 p-2" key={movie.title}>
-                 <div className="relative overflow-hidden rounded-3xl border border-white shadow-md">
-  <img className="w-full h-full object-cover  rounded-3xl border border-white shadow-md" loading="lazy" src={movie.poster} alt={movie.title} width="626" height="417" rel="preload"  priority={true} />
+      <main className={styles.main}>
+  <section className={`${styles.movies} bg-gray-600 shadow md:block py-5`}>
+  <ShareButtons url="https://uwatchfree.vercel.app" title="Watch Movies, TV-Series & Sports Live Online Free" image="https://uwatchfree.vercel.app/og_image.jpg" />
+    <h2 className="text-center font-bold text-3xl text-white py-5" style={{ color: "#40D7BC", textShadow: "5px 5px 2px #000" }}>Watch Movies, TV-Series & Sports Live Online Free</h2>
+    <div className="flex flex-wrap justify-center">
+      {Array.isArray(movies) && movies.slice(1).map((movie, index) => (
+        <div className="w-full md:w-1/2 lg:w-1/3 p-2" key={movie.title}>
+        
+    <div className="relative overflow-hidden rounded-3xl border border-white shadow-md">
+  <img className="w-full h-full object-cover rounded-3xl border border-white shadow-md" loading="lazy" src={movie.poster} alt={movie.title} width="626" height="417" rel="preload" priority={true} />
+  <a href={movie['movie.watch']} className="absolute inset-0 flex items-center justify-center"></a>
 
-  <a href={movie['movie.watch']} className="absolute inset-0 flex items-center justify-center  "  >
-   
-  </a>
+
+  <span className={`${movie.status === 'New Movie' || movie.badge === 'New Movie' ? 'bg-green-500 border border-white' : movie.status === 'New Series' || movie.badge === 'New Series' ? 'bg-yellow-500 border border-white' : movie.status === 'Updated' || movie.badge === 'Updated' ? 'bg-yellow-500 border border-white' : movie.status === 'Live Sports' || movie.badge === 'Live Sports' ? 'bg-red-500 border border-white' : movie.status === 'Tv Show' || movie.badge === 'Tv Show' ? movie.badge === 'blue' ? 'bg-blue-500 border border-white' : 'bg-blue-500 border border-white' : movie.status === 'Sports' || movie.badge === 'Sports' ? movie.badge === 'orange' ? 'bg-orange-500 border border-white' : 'bg-orange-500 border border-white' : ''} text-black font-bold py-2 px-4 rounded-3xl absolute top-0 right-0 m-1 animate-pulse ${movie.status === 'new' || movie.badge === 'new' ? '-slow' : ''}`}>
+  {movie.status || movie.badge}
+</span>
+
+
+
+
+
 </div>
-                  <div className="mt-4">
-                  <h1 className="text-2xl font-bold leading-normal mb-2" style={{ color: "#40D7BC", textShadow: "5px 5px 2px #000" }}>
-  {movie.title} 
-</h1>
 
 
-                    <a href={movie.link} className="text-xl font-bold leading-normal mb-2 text-yellow-500 " target="_blank" style={{ textShadow: "2px 2px 4px #000" }}>IMDb Rating : {movie.rating}</a>     
-                    <p className={`${styles.yearRelease} text-xl font-bold leading-normal mb-2 text-white`} style={{ textShadow: "5px 5px 2px #000" }}>Year Release : {movie.yearRelease}</p>
-                    <p className={`${styles.genre} text-xl font-bold leading-normal mb-2 text-white`} style={{ textShadow: "5px 5px 2px #000" }}>Genre : {movie.genre}</p>
-                  </div> 
-                </div>
-              ))}
-            </div>
-          </section>
-        </main>
-
-       
+          <div className="mt-4">
+            <h1 className="text-2xl font-bold leading-normal mb-2" style={{ color: "#40D7BC", textShadow: "5px 5px 2px #000" }}>
+              {movie.title}
+            </h1>
+            <a href={movie.link} className="text-xl font-bold leading-normal mb-2 text-yellow-500 " target="_blank" style={{ textShadow: "2px 2px 4px #000" }}>IMDb Rating : {movie.rating}</a>
+            <p className={`${styles.yearRelease} text-xl font-bold leading-normal mb-2 text-white`} style={{ textShadow: "5px 5px 2px #000" }}>Year Release : {movie.yearRelease}</p>
+            <p className={`${styles.genre} text-xl font-bold leading-normal mb-2 text-white`} style={{ textShadow: "5px 5px 2px #000" }}>Genre : {movie.genre}</p>
+          </div>
+        </div>
+      ))}
+    </div>
+  </section>
+</main>
       </div>
     </div>
   );
