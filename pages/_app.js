e06157663ef6ca805c039fbf86cd92852cb5e750
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-19 21:05:43 +0530

    update maintain

--- pages/_app.js
+++ pages/_app.js
@@ -3,25 +3,40 @@ import Footer from '../components/Footer';
 import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics"
-
+import Maintenance from './maintenance';
 import Script from 'next/script';
+import React, { useState } from 'react';
+
 
 function Application({ Component, pageProps }) {
- 
-  return ( 
-    <>
-     <div className="center">
-    <GoogleAnalytics measurementId="G-K8QKRZ4B44" />
-    <Script async data-id="101405628" src="//static.getclicky.com/js"></Script>
+  const [isMaintenanceMode, setIsMaintenanceMode] = useState(true); // true is on for 3 hours
+
+  const renderContent = () => {
+    if (isMaintenanceMode) {
+      return <Maintenance setIsMaintenanceMode={setIsMaintenanceMode} />;
+    } else {
+      return (
+        <>
+          <div className="center">
+            <GoogleAnalytics measurementId="G-K8QKRZ4B44" />
+            <Script async data-id="101405628" src="//static.getclicky.com/js"></Script>
      
-      <PageTransition>  
-        <Hamburger />
-        <Component {...pageProps} />
-        <Footer />
-      </PageTransition>
-   </div>
-   </>
+            <PageTransition>  
+              <Hamburger />
+              <Component {...pageProps} />
+              <Footer />
+            </PageTransition>
+          </div>
+        </>
+      );
+    }
+  };
+
+  return (
+    <div>
+      {renderContent()}
+    </div>
   );
 }
 
-export default Application
+export default Application;
