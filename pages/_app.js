422e777917d95ad1cccaf732415bddf83b7ed2d6
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-28 00:03:34 +0530

    update

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ import Hamburger from '../components/Hamburger';
 import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 import Script from 'next/script';
-import Maintenance from './maintenance';
+
 
 function Application({ Component, pageProps }) {
 
