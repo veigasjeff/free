4fb72e5fc9546ab906196776df68db777fc48892
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-04-30 20:10:39 +0530

    Update 30 April 2023

--- pages/Hollywood/MrsDavis-2023/MDSSR4.js
+++ pages/Hollywood/MrsDavis-2023/MDSSR4.js
@@ -308,7 +308,7 @@ Download
 }
 
 export async function getServerSideProps() {
-  const res = await fetch('http://localhost:3000/movies.json');
+  const res = await fetch('https://uwatchfree.vercel.app/movies.json');
 
   const data = await res.json();
   const selectedMovie = data.find(movie => movie.id === 'MDS');
