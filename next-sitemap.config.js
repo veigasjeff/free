991bc212149e550417fe7490871d7d4f8a0a6b44
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-05-17 14:03:40 +0530

    UPDATE

--- next-sitemap.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('next-sitemap').IConfig} */
-module.exports = {
-    siteUrl: 'https://uwatchfree.vercel.app/',
-    changefreq: 'daily',
-    priority: 0.80,
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-  }
