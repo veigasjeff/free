31aafcb671d38357f19b978eb4201324bcec38f5
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-05-21 13:35:09 +0530

    UPDATE GTag

--- pages/index.js
+++ pages/index.js
@@ -152,6 +152,13 @@ const LandingPage = () => {
  
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-K8QKRZ4B44"></script>
 
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-K8QKRZ4B44');
+</script>
 
 
       </Head>
