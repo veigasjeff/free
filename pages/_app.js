2cdbf109570130d64deac66b10d5c6ea32d4ff1f
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-05-21 15:22:22 +0530

    Final GTAGS

--- pages/_app.js
+++ pages/_app.js
@@ -5,13 +5,14 @@ import { PageTransition } from "../components/PageTransition";
 import GoogleAnalytics from "@bradgarropy/next-google-analytics";
 import Script from 'next/script';
 import { initGA, logPageView } from '../components/ga';
+import React, { useEffect } from 'react';
 
 function Application({ Component, pageProps }) {
-  componentDidMount() {
+  useEffect(() => {
     initGA();
     logPageView();
-  }
-  
+  }, []);
+
   return ( 
     <>
       
