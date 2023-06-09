34462ea230ad6f00e1825e30edca21f63df8f02b
groupincworld <110382877+veigasjeff@users.noreply.github.com>
2023-06-09 23:15:17 +0530

    update

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -36,7 +36,7 @@ const BackgroundVideo = ({ src, srcMobile }) => {
           <button
       className={`relative inline-flex items-center rounded-3xl  my-5 justify-center p-0.5 mb-5 mr-2 overflow-hidden text-xl font-bold text-gray-900 group bg-gradient-to-br ${
         isBlinking ?  "from-red-600 to-blue-500" : "from-blue-500 to-red-600"
-      } group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 scale-120 hover:scale-110 cursor-pointer px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0 border-white-500 border-2`}
+      } group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 scale-120 hover:scale-110 cursor-pointer px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 group-hover:bg-opacity-0 border-red-500 border-2 `} style={{ boxShadow: '0 0 5px 5px rgba(255, 0, 0, 0.7)' }}
     >
       Enter Free
     </button>
