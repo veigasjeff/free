eafa8235ef40351dde0074303070e16ab8a96606
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-06-23 12:33:52 +0530

    UPDATE CHECK

--- pages/Bollywood/bloodydaddy-2023/Watch-Bloody-Daddy-2023-Full-Movie-Online.js
+++ pages/Bollywood/bloodydaddy-2023/Watch-Bloody-Daddy-2023-Full-Movie-Online.js
@@ -5,6 +5,7 @@ import React, { useEffect, useState } from "react";
 import styles from "@styles/video-player.module.css";
 import Max from "pages/Max";
 import ShareButtons from "@components/ShareButtons";
+import BackgroundVideo from '@components/BackgroundVideo';
 import Script from "next/script";
 import { Image } from "cloudinary-react";
 import videojs from "video.js";
@@ -180,17 +181,11 @@ function BloodyDaddy({ movie }) {
         >
           Bloody Daddy - 2023
         </h1>
-        <div className={styles["iframe-container"]}>
-          <iframe
-            className="  rounded-3xl  mr-8 flex  border-1 border-blue-600 bg-gray-600 p-2 "
-            src={movie["movie.watchP1"]}
-            width="100%"
-            height="700"
-            allowFullScreen
-            webkitallowfullscreen
-          mozallowFullScreen="true"
-          ></iframe>
-        </div>
+           
+        <BackgroundVideo movie={[movie["movie.watchP1"]]} />
+
+
+      
         <h3 className="mb-9 text-bg  text-black-500 text-red-600 text-center xl:px-4 ">
           *Note: Pls Select the in the Player to Change your Language of your
           Choice and Setting to Change the Quality of Video.
@@ -233,17 +228,9 @@ function BloodyDaddy({ movie }) {
         >
           Bloody Daddy - 2023
         </h1>
-        <div className={styles["iframe-container"]}>
-          <iframe
-            className="  rounded-3xl  mr-8 flex  border-1 border-blue-600 bg-gray-600 p-2 "
-            src={movie["movie.watchSR1"]}
-            width="100%"
-            height="700"
-            allowFullScreen
-            webkitallowfullscreen
-          mozallowFullScreen="true"
-          ></iframe>
-        </div>
+        
+        <BackgroundVideo movie={[movie["movie.watchSR1"]]} />
+
         <h3 className="mb-9 text-bg  text-black-500 text-red-600 text-center xl:px-4 ">
           *Note: Pls Select the in the Player to Change your Language of your
           Choice and Setting to Change the Quality of Video.
