e63840d98b289724f0849b23a7692bfe589a8259
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-06-23 13:53:03 +0530

    update

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -36,6 +36,7 @@ const BackgroundVideo = ({ movie }) => {
           object-fit: cover;
           width: 100%;
           height: 100%;
+          border-radius: 15px; 
         }
       `}</style>
     </div>
