4aa26d931a1cf334ccb1f017e845e4814d56f27e
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-06-23 20:55:50 +0530

    update

--- components/BackgroundVideo.js
+++ components/BackgroundVideo.js
@@ -30,6 +30,17 @@ const BackgroundVideo = ({ movie }) => {
     };
   }, [clientRendered]);
 
+  useEffect(() => {
+    const handleOrientationChange = () => {
+      window.location.reload();
+    };
+
+    window.addEventListener("orientationchange", handleOrientationChange);
+
+    return () => {
+      window.removeEventListener("orientationchange", handleOrientationChange);
+    };
+  }, []);
 
   return (
     <div className={`background-video ${styles.container}`}>
