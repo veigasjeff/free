92b0f65ac8deaa2aa4699dc6d33a6fe9da32fdf3
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-06-30 21:22:22 +0530

    update movies

--- pages/Movies.js
+++ pages/Movies.js
@@ -215,7 +215,7 @@ const schemaData   = {
   <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
   <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
   <link rel="manifest" href="/site.webmanifest"></link>
-  <link rel="canonical" href="https://uwatchfree.vercel.app/" />
+  <link rel="canonical" href="https://uwatchfree.vercel.app/Movies" />
  
       </Head>
 
