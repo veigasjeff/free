890ffc3ed86abd546e858c5790ff21a749a307a5
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-07-01 23:45:53 +0530

    up

--- pages/Bollywood/MaukaYaDhoka-2023/Watch-Mauka-Ya-Dhoka-2023-Movie.js
+++ pages/Bollywood/MaukaYaDhoka-2023/Watch-Mauka-Ya-Dhoka-2023-Movie.js
@@ -395,7 +395,7 @@ function MaukaYaDhoka({ movie }) {
 }
 
 export async function getServerSideProps() {
-  const res = await fetch("http://localhost:3000/movies.json");
+  const res = await fetch("https://uwatchfree.vercel.app/movies.json");
   const data = await res.json();
   const selectedMovie = data.find((movie) => movie.id === "INDEX93");
   return {
