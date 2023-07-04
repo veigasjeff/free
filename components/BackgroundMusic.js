2f9e8abda0b59d4799919f8b4311093d7bc59ba2
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-07-04 21:46:32 +0530

    UP

--- components/BackgroundMusic.js
+++ components/BackgroundMusic.js
@@ -1,29 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 const BackgroundMusic = () => {
+  const [isPlaying, setIsPlaying] = useState(false);
+
   useEffect(() => {
-    const audio = new Audio(
-      "https://res.cloudinary.com/db36kfuq3/video/upload/v1688484639/background-music-1_fc5ool.mp3"
-    );
+    const audio = new Audio('https://res.cloudinary.com/db36kfuq3/video/upload/v1688484639/background-music-1_fc5ool.mp3');
 
-    audio.loop = true; // Enable looping
 
-    const playMusic = () => {
-      audio.play().catch((error) => {
-        // Autoplay was prevented, handle the error here
-        console.error("Autoplay was prevented:", error);
-      });
+
+    const handleClick = () => {
+      setIsPlaying((prevIsPlaying) => !prevIsPlaying);
     };
 
-    const stopMusic = () => {
+    const handleButtonClick = () => {
+      setIsPlaying(false);
+    };
+
+    document.addEventListener("click", handleClick);
+    document.querySelectorAll("button").forEach((button) => {
+      button.addEventListener("click", handleButtonClick);
+    });
+
+    if (isPlaying) {
+      audio.play();
+      audio.loop = true; // Enable looping
+    } else {
       audio.pause();
       audio.currentTime = 0; // Reset the audio to the beginning
-    };
-
-    playMusic(); // Start playing the music when the component mounts
+    }
 
     return () => {
-      stopMusic(); // Stop the music when the component unmounts
+      audio.pause();
+      document.removeEventListener("click", handleClick);
+      document.querySelectorAll("button").forEach((button) => {
+        button.removeEventListener("click", handleButtonClick);
+      });
+    };
+  }, [isPlaying]);
+
+  useEffect(() => {
+    setIsPlaying(true); // Autoplay the music when the component mounts
+  }, []);
+
+  useEffect(() => {
+    const handleLocalClick = (event) => {
+      const clickedElement = event.target;
+      if (clickedElement.tagName === "BUTTON") {
+        setIsPlaying(false);
+      }
+    };
+
+    document.querySelectorAll("button").forEach((button) => {
+      button.addEventListener("click", handleLocalClick);
+    });
+
+    return () => {
+      document.querySelectorAll("button").forEach((button) => {
+        button.removeEventListener("click", handleLocalClick);
+      });
     };
   }, []);
 
