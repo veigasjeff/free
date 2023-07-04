6e6f73d225e3cc419dfad5b62e0fa866e008fdd9
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-07-04 21:13:58 +0530

    updated

--- components/BackgroundMusic.js
+++ components/BackgroundMusic.js
@@ -4,24 +4,21 @@ const BackgroundMusic = () => {
   const [isPlaying, setIsPlaying] = useState(false);
 
   useEffect(() => {
-    const audio = new Audio('https://res.cloudinary.com/db36kfuq3/video/upload/v1688484639/background-music-1_fc5ool.mp3');
-
+    const audio = new Audio(
+      "https://res.cloudinary.com/db36kfuq3/video/upload/v1688484639/background-music-1_fc5ool.mp3"
+    );
 
     const handleClick = () => {
       setIsPlaying((prevIsPlaying) => !prevIsPlaying);
     };
 
-    const handleButtonClick = () => {
-      setIsPlaying(false);
-    };
-
     document.addEventListener("click", handleClick);
-    document.querySelectorAll("button").forEach((button) => {
-      button.addEventListener("click", handleButtonClick);
-    });
 
     if (isPlaying) {
-      audio.play();
+      audio.play().catch((error) => {
+        // Autoplay was prevented, handle the error here
+        console.error("Autoplay was prevented:", error);
+      });
       audio.loop = true; // Enable looping
     } else {
       audio.pause();
@@ -31,34 +28,8 @@ const BackgroundMusic = () => {
     return () => {
       audio.pause();
       document.removeEventListener("click", handleClick);
-      document.querySelectorAll("button").forEach((button) => {
-        button.removeEventListener("click", handleButtonClick);
-      });
     };
   }, [isPlaying]);
-
-  useEffect(() => {
-    setIsPlaying(true); // Autoplay the music when the component mounts
-  }, []);
-
-  useEffect(() => {
-    const handleLocalClick = (event) => {
-      const clickedElement = event.target;
-      if (clickedElement.tagName === "BUTTON") {
-        setIsPlaying(false);
-      }
-    };
-
-    document.querySelectorAll("button").forEach((button) => {
-      button.addEventListener("click", handleLocalClick);
-    });
-
-    return () => {
-      document.querySelectorAll("button").forEach((button) => {
-        button.removeEventListener("click", handleLocalClick);
-      });
-    };
-  }, []);
 
   return null;
 };
