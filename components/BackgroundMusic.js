aa0adacc30e3bb9da146ddb2bc12d465b4242191
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-07-04 21:38:41 +0530

    FINAL UPDATED

--- components/BackgroundMusic.js
+++ components/BackgroundMusic.js
@@ -14,21 +14,33 @@ const BackgroundMusic = () => {
 
     audio.loop = true; // Enable looping
 
-    if (isPlaying) {
+    const playMusic = () => {
       audio.play().catch((error) => {
         // Autoplay was prevented, handle the error here
         console.error("Autoplay was prevented:", error);
       });
-    } else {
-      audio.pause();
-      audio.currentTime = 0; // Reset the audio to the beginning
-    }
+    };
 
-    return () => {
+    const stopMusic = () => {
       audio.pause();
       audio.currentTime = 0; // Reset the audio to the beginning
     };
+
+    if (isPlaying) {
+      playMusic(); // Start playing the music when isPlaying is true
+    } else {
+      stopMusic(); // Stop the music when isPlaying is false
+    }
+
+    return () => {
+      stopMusic(); // Stop the music when the component unmounts
+    };
   }, [isPlaying]);
+
+  useEffect(() => {
+    // Autoplay the music when the component mounts
+    setIsPlaying(true);
+  }, []);
 
   return (
     <button onClick={handleButtonClick}>
