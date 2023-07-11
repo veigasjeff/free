49b5d77f3e8535c6b80a986ad948417b6f1e7ea9
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-07-11 08:32:07 +0530

    update full document

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,149 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="preconnect" href="https://uwatchfree.vercel.app/" />
-          <link rel="dns-prefetch" href="https://uwatchfree.vercel.app/" />
+      
+        <meta name="robots" content="index, follow" />
+        <meta name="googlebot" content="index,follow"/>
+        <meta name="revisit-after" content="1 days" />
+        <meta
+          name="facebook-domain-verification"
+          content="13auzwhblf4oo4jn5vl6gcmebugsqb"
+        />
+        <meta property="fb:app_id" content="602176271414602" />
+        <meta
+          name="dailymotion-domain-verification"
+          content="dmv6sg06w9r5eji88"
+        />
+        <meta name="monetag" content="076afbb772da1a62ef6f43756dfa5f65"></meta>
+        <meta
+          name="google-site-verification"
+          content="4dFu4PUk1pc1IYqU6Brt84akCwNxaoUpKSO3gDW0kJ0"
+        />
+        <meta
+          name="description"
+          content="Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free"
+        />
+        <meta
+          name="robots"
+          content="max-snippet:-1, max-image-preview:large, max-video-preview:-1"
+        />
+        <meta
+          name="keywords"
+          content="uwatchfree,streaming,latest movies,online tv,latest free movies,watch latest movies online"
+        />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:locale" content="en_US" />
+        <meta property=" Content-Security-Policy: frame-ancestors 'self' uwatchfree.vercel.app *.uwatchfree.vercel.app;" />
+        <meta
+          property="og:site_name"
+          content="Uwatchfree™ | Watch Movies, TV-Series & Sports Live Online Free "
+        />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:title"
+          content="Uwatchfree™ | Watch Movies, TV-Series & Sports Live Online Free "
+        />
+        <meta
+          property="og:description"
+          content="Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free "
+        />
+        <meta property="og:url" content="https://uwatchfree.vercel.app/" />
+        <meta property="og:image:type" content="image/jpeg" />
+        <meta
+          property="og:image"
+          content="https://uwatchfree.vercel.app/og_image.jpg"
+        />
+        <meta
+          property="og:image:secure_url"
+          content="https://uwatchfree.vercel.app/og_image.jpg"
+        />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta
+          name="twitter:title"
+          content="Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free "
+        />
+        <meta
+          name="twitter:description"
+          content=" Uwatchfree™ - Watch Movies, TV-Series & Sports Live Online Free "
+        />
+        <meta
+          name="twitter:image"
+          content="https://uwatchfree.vercel.app/og_image.jpg"
+        />
+
+        <link
+          rel="alternate"
+          hrefLang="en-us"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-gb"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-ca"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-au"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-se"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-fr"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-dk"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="en-no"
+          href="https://uwatchfree.vercel.app/"
+        />
+        <link
+          rel="alternate"
+          hrefLang="x-default"
+          href="https://uwatchfree.vercel.app/"
+        />
+
+        <link rel="icon" type="image/x-icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest"></link>
+        <link
+          rel="preload"
+          href="https://res.cloudinary.com/dezf3wemk/image/upload/w_1000,ar_16:9,c_fill,g_auto,e_sharpen/v1681610087/og_image_okmgde.jpg"
+          as="image"
+        />
+        <link rel="canonical" href="https://uwatchfree.vercel.app/" />
+        <link rel="preconnect" href="https://uwatchfree.vercel.app/" />
+        <link rel="dns-prefetch" href="https://uwatchfree.vercel.app/" />
         </Head>
         <body>
           <Main />
