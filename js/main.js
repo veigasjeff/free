2d4029c749881f9ca0fc3348841ea317bb6804f0
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-08-09 09:01:54 +0530

    updated json

--- js/main.js
+++ js/main.js
@@ -99,6 +99,31 @@ fetch("/movies.json")
       }
     }
 
-    
+    function shareSocialMedia(platform) {
+      const parentURL = window.location.href;
+
+      switch (platform) {
+          case "facebook":
+              window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(parentURL)}`);
+              break;
+          case "twitter":
+              window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(parentURL)}&text=Check%20out%20this%20awesome%20movie%20content!`);
+              break;
+          case "linkedin":
+              window.open(`https://www.linkedin.com/shareArticle?url=${encodeURIComponent(parentURL)}`);
+              break;
+          case "whatsapp":
+              window.open(`whatsapp://send?text=Check%20out%20this%20awesome%20movie%20content!%20${encodeURIComponent(parentURL)}`);
+              break;
+          case "email":
+              window.location.href = `mailto:?subject=Check%20out%20this%20awesome%20movie%20content!&body=Check%20out%20this%20awesome%20content!%20${encodeURIComponent(parentURL)}`;
+              break;
+          default:
+              console.log("Unsupported platform.");
+      }
+  }
+
+
+
 
     (function (s, u, z, p) { s.src = u, s.setAttribute('data-zone', z), p.appendChild(s); })(document.createElement('script'), 'https://inklinkor.com/tag.min.js', 5741273, document.body || document.documentElement);
