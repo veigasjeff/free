728a8bae4416e6cd3f04f80622ce4937ed493c39
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-08-29 09:05:43 +0530

    updated googletag

--- wp-content/plugins/googlescript.js
+++ wp-content/plugins/googlescript.js
@@ -3,7 +3,7 @@
 (function () {
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-9VBW9JPWQZ';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-K8QKRZ4B44';
   
     document.head.appendChild(script);
   
@@ -11,7 +11,7 @@
       window.dataLayer = window.dataLayer || [];
       function gtag() { dataLayer.push(arguments); }
       gtag('js', new Date());
-      gtag('config', 'G-9VBW9JPWQZ');
+      gtag('config', 'G-K8QKRZ4B44');
     };
   })();
   
