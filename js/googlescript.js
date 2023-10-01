16a94180b087399e33e43d6b6307ff97d71247fd
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-10-01 07:23:05 +0530

    UPDATE GTAG

--- js/googlescript.js
+++ js/googlescript.js
@@ -3,7 +3,7 @@
 (function () {
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-H3XPWBEBJV';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MZZHBNCTCN';
   
     document.head.appendChild(script);
   
@@ -11,7 +11,7 @@
       window.dataLayer = window.dataLayer || [];
       function gtag() { dataLayer.push(arguments); }
       gtag('js', new Date());
-      gtag('config', 'G-H3XPWBEBJV');
+      gtag('config', 'G-MZZHBNCTCN');
     };
   })();
   
