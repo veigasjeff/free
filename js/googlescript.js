ac33e9bb2ec0ae93bee2668b214d3ee72697eab0
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-10-02 08:00:25 +0530

    gs tag

--- js/googlescript.js
+++ js/googlescript.js
@@ -3,7 +3,7 @@
 (function () {
     var script = document.createElement('script');
     script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-K8QKRZ4B44';
+    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-MZZHBNCTCN';
   
     document.head.appendChild(script);
   
@@ -11,7 +11,7 @@
       window.dataLayer = window.dataLayer || [];
       function gtag() { dataLayer.push(arguments); }
       gtag('js', new Date());
-      gtag('config', 'G-K8QKRZ4B44');
+      gtag('config', 'G-MZZHBNCTCN');
     };
   })();
   // K8QKRZ4B44
