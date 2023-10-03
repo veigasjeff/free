c2e980d5b370343e5161379e93017b672cf928df
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-10-03 20:29:53 +0530

    update piwik

--- js/tracker.js
+++ js/tracker.js
@@ -9,10 +9,10 @@ tags.async=!0,tags.src="https://uwatchfree.containers.piwik.pro/"+id+".js"+qPStr
 })(window, document, 'dataLayer', 'e942a239-e7e9-4562-9cc9-43efb581bd63');
 
 
-(function(window, document, dataLayerName, id) {
-function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString();f="; SameSite=Strict"}document.cookie=a+"="+b+d+f+"; path=/"}
-var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
-var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
-document.write('<script src="https://uwatchfree.containers.piwik.pro/'+id+'.sync.js' + qPString + '"></' + 'script>');
-})(window, document, 'dataLayer', 'e942a239-e7e9-4562-9cc9-43efb581bd63');
+// (function(window, document, dataLayerName, id) {
+// function stgCreateCookie(a,b,c){var d="";if(c){var e=new Date;e.setTime(e.getTime()+24*c*60*60*1e3),d="; expires="+e.toUTCString();f="; SameSite=Strict"}document.cookie=a+"="+b+d+f+"; path=/"}
+// var isStgDebug=(window.location.href.match("stg_debug")||document.cookie.match("stg_debug"))&&!window.location.href.match("stg_disable_debug");stgCreateCookie("stg_debug",isStgDebug?1:"",isStgDebug?14:-1);
+// var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName),isStgDebug&&qP.push("stg_debug");var qPString=qP.length>0?("?"+qP.join("&")):"";
+// document.write('<script src="https://uwatchfree.containers.piwik.pro/'+id+'.sync.js' + qPString + '"></' + 'script>');
+// })(window, document, 'dataLayer', 'e942a239-e7e9-4562-9cc9-43efb581bd63');
 
