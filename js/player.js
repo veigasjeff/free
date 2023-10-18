a494a7b1860644b659f0ad26c2e9928b5fb402d9
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-10-18 21:52:02 +0530

    update deleted

--- js/player.js
+++
@@ -1,8 +0,0 @@
-
-  // Register the Hls.js tech so it can be used in the video.js player
-  videojs.registerTech('html5.hlsjs', videojs.getComponent('Html5'));
-
-  // Enable the hlsjs tech
-  videojs('my-video', {
-    techOrder: ['html5', 'hlsjs'],
-  });
