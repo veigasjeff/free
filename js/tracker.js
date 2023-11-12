d4942629d3401934ca8d0bdcfafd0c5ad4702a81
drtrailer2022 <110382877+veigasjeff@users.noreply.github.com>
2023-11-13 00:32:27 +0530

    UPDATED G TAGS

--- js/tracker.js
+++ js/tracker.js
@@ -1,17 +1,5 @@
-// googlescript.js
-
-(function () {
-    var script = document.createElement('script');
-    script.async = true;
-    script.src = 'https://www.googletagmanager.com/gtag/js?id=G-K8QKRZ4B44';
-  
-    document.head.appendChild(script);
-  
-    script.onload = function () {
-      window.dataLayer = window.dataLayer || [];
-      function gtag() { dataLayer.push(arguments); }
-      gtag('js', new Date());
-      gtag('config', 'G-K8QKRZ4B44');
-    };
-  })();
-  // K8QKRZ4B44
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-KD4HLJCH');
